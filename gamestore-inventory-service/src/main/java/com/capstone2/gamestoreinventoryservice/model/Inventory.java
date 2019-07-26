e32901dd0b43de578a2377401b46b5aa8f38ab21
Arturo Salmeron <asalmeron4@dhcp-10-101-246-34.wireless.northwestern.private>
2019-07-26 16:56:35 -0500

    Adding Inventory service

---
+++ gamestore-inventory-service/src/main/java/com/capstone2/gamestoreinventoryservice/model/Inventory.java
@@ -0,0 +1,72 @@
+package com.capstone2.gamestoreinventoryservice.model;
+
+import javax.validation.constraints.Positive;
+import java.util.Objects;
+
+public class Inventory {
+
+    private int inventoryId;
+
+    @Positive(message = "'productId' must be present in this inventory")
+    private int productId;
+
+    @Positive(message = "'quantity' must be present in this inventory")
+    private int quantity;
+
+    public Inventory() {
+
+    }
+
+    public Inventory(int productId, int quantity) {
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public int getInventoryId() {
+        return inventoryId;
+    }
+
+    public void setInventoryId(int inventoryId) {
+        this.inventoryId = inventoryId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Inventory inventory = (Inventory) o;
+        return inventoryId == inventory.inventoryId &&
+                productId == inventory.productId &&
+                quantity == inventory.quantity;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(inventoryId, productId, quantity);
+    }
+
+    @Override
+    public String toString() {
+        return "Inventory{" +
+                "inventoryId=" + inventoryId +
+                ", productId=" + productId +
+                ", quantity=" + quantity +
+                '}';
+    }
+}
